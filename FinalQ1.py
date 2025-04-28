2e2db3ce3938168ddb97c0390b85bec910f93cc7
Carson Davis <mdavyyy@gmail.com>
2025-04-28 11:00:01 -0600

    Add files via upload

---
+++ FinalQ1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+TAX = 0.06
+
+def sales_tax(t)
+    sales_tax = total * tax
+    return total
+
+def main():
+    print("Sales Tax Calculator\n")
+    Total = input("Enter total: ")
+    total_after_tax = round(total + sales_tax(total), 2)
+    print("Total after tax: ", total_aftertax)
+    
+if _name_ == "__main__":
+    MAIN()
